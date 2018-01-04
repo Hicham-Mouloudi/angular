aa33b3bbd25756943a29653fedc6898a85d06549
Hicham Mouloudi <mr.m.hicham@gmail.com>
2018-01-04 23:52:23 +0000

    add zoomIn animation in startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,19 @@ import * as kf from './keyframes';
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('cardAnimator', [
-      transition('* => swing', animate(1000, keyframes(kf.swing)))
+      transition('* => swing', animate(1000, keyframes(kf.swing))),
+      transition('* => zoomIn', animate(1000, keyframes(kf.zoomIn)))
     ])
   ]
 })
 export class AppComponent {
   animationState: string;
+
+  constructor(){
+    this.startAnimation("zoomIn");
+  }
+
   startAnimation(state) {
-    console.log(state)
     if (!this.animationState) {
       this.animationState = state;
     }
--- src/app/keyframes.ts
+++ src/app/keyframes.ts
@@ -1,6 +1,7 @@
 // keyframes.ts Angular code
 import { keyframes, style } from '@angular/animations';
 
+//https://github.com/daneden/animate.css/tree/master/source
 export const swing = [
     style({transform: 'rotate3d(0, 0, 1, 15deg)', offset: .2}),
     style({transform: 'rotate3d(0, 0, 1, -10deg)', offset: .4}),
@@ -8,3 +9,10 @@ export const swing = [
     style({transform: 'rotate3d(0, 0, 1, -5deg)', offset: .8}),
     style({transform: 'none', offset: 1})
 ]
+
+export const zoomIn = [
+    style({transform: 'scale3d(.3, .3, .3)', opacity: 0}),
+    style({transform: 'scale3d(1, 1, 1)', opacity: 1}),
+]
+
+ 
